e517425c70dcc87a9bf17ad27ff5468f300c2905
danielkurtsawyer <19danielsawyer@gmail.com>
2024-05-15 13:22:54 -0400

    Set up development environment

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js'
+
+export default [
+    js.configs.recommended,
+
+    {
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-undef': 'warn',
+        },
+    },
+    {
+        overrides: [
+            {
+                files: ['tests/**/*'],
+                env: {
+                    jest: true,
+                },
+            },
+        ],
+    },
+]
