1e8da658a1ca832a79b8b146512b85387cf24624
danielkurtsawyer <19danielsawyer@gmail.com>
2024-05-29 21:04:50 -0400

    Begin implementing layout and page creation with JS and CSS

--- src/scripts/DOMController.js
+++ src/scripts/DOMController.js
@@ -11,6 +11,44 @@ const loadPage = () => {
     document.body.appendChild(header);
     document.body.appendChild(main);
     document.body.appendChild(footer);
+
+    // add header text
+    header.textContent = 'BATTLESHIP';
+
+    // add hyperlink to GitHub page
+    const gitHubLink = document.createElement('a');
+    gitHubLink.textContent = 'Github';
+    gitHubLink.href =
+        'https://github.com/danielkurtsawyer/odin-javascript-battleship';
+    gitHubLink.target = '_blank';
+    gitHubLink.rel = 'noopener noreferrer';
+
+    // append link to footer
+    footer.appendChild(gitHubLink);
+
+    // make left and right divs within the main-wrapper
+    const left = document.createElement('div');
+    left.classList.add('main-wrapper-left');
+
+    const right = document.createElement('div');
+    right.classList.add('main-wrapper-right');
+
+    // append to the main-wrapper
+    main.appendChild(left);
+    main.appendChild(right);
+
+    // add divs for the boards
+    const playerBoard = document.createElement('div');
+    playerBoard.classList.add('player-board');
+
+    const opponentBoard = document.createElement('div');
+    opponentBoard.classList.add('opponent-board');
+
+    // append the playerboard to the left div
+    left.appendChild(playerBoard);
+
+    // append the opponentboard to the right div
+    right.appendChild(opponentBoard);
 };
 
 export { loadPage };
